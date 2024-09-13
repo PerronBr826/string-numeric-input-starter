cdb3090d9e5f7b2c6fdd1ce67017d3eb17cbc4c1
PerronBr826 <PerronBr826@northwested.org>
2024-09-13 10:05:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-# Student name
-# Current date
+# Bryce Perron
+# 9/13/2024
 # String & Numeric Input 
 
 # Task 1
 # Comment
 # Comment
-
+name = input('What is your name?\n').title()
+program = input('What is the program you attend at Career Tech?\n')
+print(f'{name} attends {program} at Career Tech.')
 
 
 
@@ -13,7 +15,8 @@
 # Comment
 # Comment
 
-
+age = str(input('What is your age?\n'))
+print(f'In ten years {name} will be {age}')
 
 
 # Task 3
